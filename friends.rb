a14e54d00f9c4e84cc8cf4363de2716a6ad309a1
Matthew Woodley <matthewwoodley@Matthews-MacBook-Pro.local>
2019-01-17 14:30:37 +0000

    test 2 done

--- friends.rb
+++ friends.rb
@@ -1,3 +1,16 @@
 def get_name(person)
   return person[:name]
 end
+
+def get_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def favourite_food_true(person, food)
+  for snacks in person[:favourites][:snacks]
+    if snacks == food
+      return true
+    end
+  end
+  return false
+end
